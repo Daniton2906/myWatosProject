aa3beaa34ccfec0fcae064a2e480883e73faef8c
Daniel <ddiomedi2906@gmail.com>
2018-12-09 20:00:46 -0300

    Finish getArtists

--- getArtists.py
+++ getArtists.py
@@ -21,103 +21,99 @@ if not os.path.exists("data/token.txt"):
 tk = open("data/token.txt", "r")
 MY_TOKEN = tk.readline()
 
-# release_keys = ['user_data', 'community', 'catno', 'year', 'id', 'style', 'thumb', 'title', 'label', 'master_id', 'type', 'format', 'barcode', 'master_url', 'genre', 'country', 'uri', 'cover_image', 'resource_url', 'status', 'videos', 'series', 'labels', 'artists', 'images', 'format_quantity', 'artists_sort', 'genres', 'num_for_sale', 'date_changed', 'lowest_price', 'styles', 'released_formatted', 'formats', 'estimated_weight', 'released', 'date_added', 'extraartists', 'tracklist', 'notes', 'identifiers', 'companies', 'data_quality']
-artists_keys = ['id',
-                'title',
-                'labels',
-                'master_id',
-                'country',
-                'year',
-                'released',
-                'genre',
-                'style',
-                'artists',
-                'tracklist',
-                'uri']
+# ARTIST_KEYS = ['thumb', 'title', 'user_data', 'master_url', 'uri', 'cover_image', 'resource_url', 'master_id', 'type', 'id', 'name']
+ARTIST_KEYS = ['id', 'name', 'realname', 'members', 'uri']
 
-TICK = 5 # 50
-SLEEP_TIME = 1 # 120
+TICK = 15
+SLEEP_TIME = 65 #
+
 
 def writeArtists(d, encoding='utf-8'):
     year = 2008
-    suma = 0
-    for i in range(10):
-        print("Artist for year " + str(year + i) + "in Chile.")
-        suma += writeInfo(d, "artist", str(year + i) ,encoding)
+    visited_artists = []
+    for i in range(2): # (10):
+        print("Artist for year " + str(year + i) + " in Chile.")
+        writeInfo(d, visited_artists, str(year + i), encoding)
         print("Get to sleep for {}s at: {}".format(SLEEP_TIME, datetime.datetime.now()))
         time.sleep(SLEEP_TIME)
         print("Wake up at {} ...".format(datetime.datetime.now()))
-    print("Total artists: " + str(suma))
-    return suma
+    print("Total artists: " + str(len(visited_artists)))
+    return len(visited_artists)
+
+
 
 def writeEncoded(fd, s, encoding):
     fd.write(s.encode(encoding))
 
-def writeInfo(d, search_type, search_year, encoding):
-    results = d.search('*', type=search_type)
-    count_results = results.count
-    print("Count: ", count_results)
-    # print(results)
-    with io.open("data/artists" + search_year + ".csv", 'wb+') as fd_a:
-        header_a = ""
-        for column in artists_keys:
-            header_a += column + '\t'
-        header_a = header_a[:-1] + '\n'
-        writeEncoded(fd_a, header_a, encoding)
-        count = 0
-        try:
-            for element in results:
-                writeResults(fd_a, element, encoding)
-                count += 1
-                print("read ...{} lines".format(count))
-                if count % TICK == 0:
-                    print("Get to sleep for {}s at: {}".format(SLEEP_TIME, datetime.datetime.now()))
-                    time.sleep(SLEEP_TIME)
-                    print("Wake up at {} ...".format(datetime.datetime.now()))
-        except Exception as e:
-            print("ups")
-            print(e)
-            traceback.print_exc()
-        finally:
-            fd_a.close()
-    return count_results
 
-def stringList(data, list_key=None):
+def writeInfo(d, visited_artists, search_year, encoding):
+    # results = d.search('*', type=search_type)
+    # count_results = results.count
+    # print("Count: ", count_results)
+    # print(results)
+    with io.open("data/releases" + search_year + ".csv", 'rb') as fd_r:
+        with io.open("data/artists" + search_year + ".csv", 'wb+') as fd_a:
+            header_a = ""
+            for column in ARTIST_KEYS:
+                header_a += column + '\t'
+            header_a = header_a[:-1] + '\n'
+            writeEncoded(fd_a, header_a, encoding)
+            count = 0
+            fd_r.readline()
+            for line in fd_r:
+                line = line.decode(encoding)
+                artists = line.split('\t')[-3].split(',')
+                for artist_id in artists:
+                    count += 1
+                    try:
+                        writeResults(fd_a, d, artist_id, visited_artists, encoding)
+                    except Exception as e:
+                        print("ups")
+                        print(e)
+                        traceback.print_exc()
+                    print("read ...{} lines".format(count))
+                    if count % TICK == 0:
+                        print("Get to sleep for {}s at: {}".format(SLEEP_TIME, datetime.datetime.now()))
+                        time.sleep(SLEEP_TIME)
+                        print("Wake up at {} ...".format(datetime.datetime.now()))
+                        return
+    return
+
+def writeMembers(data, write):
     s = ""
     for entity in data:
-        if list_key == "tracklist":
-            s += "<{} {}>,".format(entity['position'], entity['title'])
-        elif list_key is not None:
-            s += str(entity[list_key]) + ","
-        else:
-            s += str(entity) + ","
+        write(entity['id'])
+        s += str(entity['id']) + ","
     # Escribir en archivo la informacion
     s = s[:-1]
     # print(s)
     return s
 
 
-def writeResults(fd_artists, artist, encoding='utf-8'):
+def writeResults(fd_artists, d, artist_id, visited_artists, encoding='utf-8'):
+    if artist_id in visited_artists:
+        return
+    else:
+        visited_artists.append(artist_id)
+    artist = d.artist(artist_id)
     # Para los lanzamientos en Chile
     s_artist = ""
     # Escribir informacion de lanzamiento
     my_keys = artist.data.keys()
-    print(my_keys)
-    for key in my_keys: #artists_keys:
-        print("{}: {}".format(key, artist.data[key]))
-        continue
+    print(artist.name)
+    # print(my_keys)
+    for key in ARTIST_KEYS:  # artists_keys:
         if key not in my_keys:
             s_artist += "" + '\t'
-        elif key in ["labels", "artists"]:
-            s_artist += stringList(artist.data[key], list_key="id") + '\t'
-        elif key in ["genre", "style"]:
-            s_artist += stringList(artist.data[key]) + '\t'
-        elif key == "tracklist":
-            s_artist += stringList(artist.data[key], list_key="tracklist") + '\t'
+            continue
+        elif key == "members":
+            f = lambda x: writeResults(fd_artists, d, x, visited_artists)
+            s_artist += writeMembers(artist.data[key], f) + '\t'
         else:
             s_artist += str(artist.data[key]) + "\t"
-    print("################################################################################")
-    # writeEncoded(fd_artists, s_artist[:-1] + "\n", encoding)
+        # print("{}: {}".format(key, artist.data[key]))
+    # print("################################################################################")
+    writeEncoded(fd_artists, s_artist[:-1] + "\n", encoding)
 
 
 d = discogs_client.Client('ExampleApplication/0.1', user_token=MY_TOKEN)
@@ -125,6 +121,3 @@ d = discogs_client.Client('ExampleApplication/0.1', user_token=MY_TOKEN)
 # getReleases(d)
 # printSample(d, 'release', keys=release_keys)
 writeArtists(d)
-
-
-
