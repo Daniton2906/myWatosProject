e7a804f0a58eb15376c6a6bdf5d22cc4a7fded9f
Daniel <ddiomedi2906@gmail.com>
2018-12-09 10:47:46 -0300

    Search releases in Chile from 2008 to 2017

--- getReleases.py
+++ getReleases.py
@@ -7,6 +7,8 @@ import datetime
 
 import discogs_client
 
+import traceback
+
 from utils import printSample
 
 if not os.path.isdir("data"):
@@ -21,32 +23,48 @@ MY_TOKEN = tk.readline()
 
 # release_keys = ['user_data', 'community', 'catno', 'year', 'id', 'style', 'thumb', 'title', 'label', 'master_id', 'type', 'format', 'barcode', 'master_url', 'genre', 'country', 'uri', 'cover_image', 'resource_url', 'status', 'videos', 'series', 'labels', 'artists', 'images', 'format_quantity', 'artists_sort', 'genres', 'num_for_sale', 'date_changed', 'lowest_price', 'styles', 'released_formatted', 'formats', 'estimated_weight', 'released', 'date_added', 'extraartists', 'tracklist', 'notes', 'identifiers', 'companies', 'data_quality']
 release_keys = ['id',
-                'uri',
                 'title',
                 'labels',
                 'master_id',
                 'country',
                 'year',
                 'released',
-                'genres',
-                'styles',
+                'genre',
+                'style',
                 'artists',
-                'tracklist']
+                'tracklist',
+                'uri']
 
 track_keys = ['position', 'title', 'duration', 'artists']
 
-TICK = 5 # 450
-SLEEP_TIME = 1 # 120
+TICK = 50
+SLEEP_TIME = 120
 
 def writeReleases(d, encoding='utf-8'):
-    results = d.search('*', type='release', country="chile|Chile")
+    year = 2008
+    for i in range(10):
+        print("Release for year " + str(year + i))
+        writeInfo(d, "release", str(year + i) ,encoding)
+
+def writeEncoded(fd, s, encoding):
+    fd.write(s.encode(encoding))
+
+def writeInfo(d, search_type, search_year, encoding):
+    results = d.search('*', type=search_type, country="Chile", year=search_year)
     print("Count: ", results.count)
     # print(results)
-    with io.open("data/releases.csv", 'wb') as fd_r:
-        header = ""
-
-        with io.open("data/tracks.csv", 'wb+') as fd_t:
-            # fd = open("data/releases.csv", "w")
+    with io.open("data/releases" + search_year + ".csv", 'wb') as fd_r:
+        header_r = ""
+        for column in release_keys:
+            header_r += column + '\t'
+        header_r = header_r[:-1] + '\n'
+        writeEncoded(fd_r, header_r, encoding)
+        with io.open("data/tracks" + search_year + ".csv", 'wb+') as fd_t:
+            header_t = "release_id\t"
+            for column in track_keys:
+                header_t += column + '\t'
+            header_t = header_t[:-1] + '\n'
+            writeEncoded(fd_t, header_t, encoding)
             count = 0
             try:
                 for element in results:
@@ -61,28 +79,54 @@ def writeReleases(d, encoding='utf-8'):
             except Exception as e:
                 print("ups")
                 print(e)
+                traceback.print_exc()
             finally:
                 fd_t.close()
                 fd_r.close()
 
+def stringList(data, list_key=None):
+    s = ""
+    for entity in data:
+        if list_key == "tracklist":
+            s += "<{} {}>,".format(entity['position'], entity['title'])
+        elif list_key is not None:
+            s += str(entity[list_key]) + ","
+        else:
+            s += str(entity) + ","
+    # Escribir en archivo la informacion
+    s = s[:-1]
+    # print(s)
+    return s
+
 
 def writeResults(fd_releases, fd_tracks, release, encoding='utf-8'):
+    # Para los lanzamientos en Chile
     if release.country.lower() == "chile":
+        # Obtener lista de canciones de dicho lanzamiento
         for track in release.tracklist:
             s_artists = ""
-            print(track.artists)
             for artist in track.artists:
                 s_artists += str(artist.id) + ","
-            s_tracks = "{}\t{}\t{}\t{}\t{}\n".format(release.id, track.title, track.position, track.duration, s_artists[:-1])
-            fd_tracks.write(s_tracks.encode(encoding))
+            # Si no habian artistas, solo se deja en null
+            s_tracks = "{}\t{}\t{}\t{}\t{}\n".format(release.id, track.position, track.title, track.duration, s_artists[:-1])
+            # Escribir en archivos de canciones
+            writeEncoded(fd_tracks, s_tracks, encoding)
         s_release = ""
+        # Escribir informacion de lanzamiento
+        my_keys = release.data.keys()
         for key in release_keys:
-            print("{}: {}".format(key, release.data[key]))
-        # print(release.data.keys())
-        #s = "{}\t{}\t{}\t{}\n".format(release.country, release.id, release.title, release.tracklist)
-        #print(s)
-    # print(s.encode(encoding))
-    # fd.write(s.encode(encoding))
+            if key not in my_keys:
+                s_release += "" + '\t'
+            elif key in ["labels", "artists"]:
+                s_release += stringList(release.data[key], list_key="id") + '\t'
+            elif key in ["genre", "style"]:
+                s_release += stringList(release.data[key]) + '\t'
+            elif key == "tracklist":
+                s_release += stringList(release.data[key], list_key="tracklist") + '\t'
+            else:
+                s_release += str(release.data[key]) + "\t"
+            # print("{}: {}".format(key, release.data[key]))
+        writeEncoded(fd_releases, s_release[:-1] + "\n", encoding)
 
 
 d = discogs_client.Client('ExampleApplication/0.1', user_token=MY_TOKEN)
